866a7c0531ec9e635cef6e9103a3153d7a1bc9f9
Suryansh Chaurasia <csuryansh23@gmail.com>
2022-09-23 11:38:39 +0530

    updated msg 1 to 2

--- src/main/java/com/example/utility/Util.java
+++ src/main/java/com/example/utility/Util.java
@@ -3,10 +3,11 @@ package com.example.utility;
 public class Util {
 
     public static void sayHello() {
-        System.out.println("Hello from utility repo 1");
+        System.out.println("Hello from utility repo 2");
     }
 
     public void sayGoodBye() {
-        System.out.println("GoodBye from utility repo 1");
+        System.out.println("GoodBye from utility repo 2");
     }
 }
+
