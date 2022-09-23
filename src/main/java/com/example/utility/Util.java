4b8ae2d3168e628fbd1bf2135249adf7cb6fd4df
Suryansh Chaurasia <csuryansh23@gmail.com>
2022-09-23 10:58:33 +0530

    added util class

---
+++ src/main/java/com/example/utility/Util.java
@@ -0,0 +1,12 @@
+package com.example.utility;
+
+public class Util {
+
+    public static void sayHello() {
+        System.out.println("Hello from utility repo 1");
+    }
+
+    public void sayGoodBye() {
+        System.out.println("GoodBye from utility repo 1");
+    }
+}
